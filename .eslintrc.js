851ed942be6810176fec56773adf70394c188129
Lisa Xu <Lisax@dataprocessors.com.au>
2022-09-30 10:40:59 +1000

    feat: static header and footer component

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "parserOptions": {
+        ecmaVersion: 2015,
+        "sourceType": "module"
+    },
+    "extends": ["plugin:vue/base"],
+    rules: { 'vue/multi-word-component-names': 0 }
+}
